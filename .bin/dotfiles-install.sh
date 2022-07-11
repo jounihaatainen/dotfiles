9b0a585efeb32682830e3330454968970fa26e8b
Jouni Haatainen <jouni.haatainen@gmail.com>
2022-07-11 14:20:11 +0300

    Add shebang to dotfiles-install.sh

--- .bin/dotfiles-install.sh
+++ .bin/dotfiles-install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 git clone --bare git@github.com:jounihaatainen/dotfiles.git $HOME/.dotfiles
 
 function dotfiles {
@@ -8,11 +10,13 @@ mkdir -p .dotfiles-backup
 dotfiles checkout
 
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
+  echo "Checked out dotfiles";
   else
-    echo "Backing up pre-existing dot files.";
+    echo "Backing up pre-existing dot files";
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
+
+echo "Installing dotfiles done"
