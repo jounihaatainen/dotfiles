283557f45ef45a3d2d2217c52ce39c285c1518a5
Jouni Haatainen <jouni.haatainen@gmail.com>
2023-01-12 14:08:56 +0200

    Log extra info if Hammerspoon haven't been given correct rights

--- .hammerspoon/window-layout.lua
+++ .hammerspoon/window-layout.lua
@@ -2,13 +2,17 @@ local M = {}
 
 function M.setup(settings)
   local m = hs.hotkey.modal.new(settings.enter.modifiers, settings.enter.key, nil)
+  local logger = hs.logger.new('window-layout', 'debug')
 
   hs.fnutils.each(settings.layouts, function(entry)
-    m:bind('', entry.key, nil, function()
+    m:bind('', entry.key, function()
+      if hs.window.focusedWindow() == nil then
+        logger.e("Please, turn on the Accessibility for Hammerspoon in System Settings > Privacy & Security > Accessibility")
+      end
       hs.window.focusedWindow():moveToUnit(entry.unit)
       m:exit()
     end)
-    m:bind(settings.moveToScreenModifier, entry.key, nil, function()
+    m:bind(settings.moveToScreenModifier, entry.key, function()
       hs.window.focusedWindow():moveToUnit(entry.unit)
         :moveToScreen(hs.window.focusedWindow():screen():next())
       m:exit()
