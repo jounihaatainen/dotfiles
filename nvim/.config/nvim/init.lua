3e4af89916131efcad9a00fdc0c34cd7c968051d
Jouni Haatainen <jouni.haatainen@gmail.com>
2024-07-25 19:57:38 +0300

    Remove telescope stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,20 +30,6 @@ local plugins = {
     build = ":TSUpdate",
     config = function() require("config.treesitter").setup() end
   },
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   branch = "0.1.x",
-  --   cmd = "Telescope",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     {
-  --       "nvim-telescope/telescope-fzf-native.nvim",
-  --       build =
-  --       "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-  --     }
-  --   },
-  --   config = function() require("config.telescope").setup() end
-  -- },
   {
     'junegunn/fzf.vim',
     dependencies = { 'junegunn/fzf' },
